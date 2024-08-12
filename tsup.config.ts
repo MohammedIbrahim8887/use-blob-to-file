5cc738d4ebdc55f303650feddc23d26b52a15bae
Mohammed Ibrahim <agentmame@gmail.com>
2024-08-12 18:44:34 +0300

    chore: changed bundler to bun, moved react to devDependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
  
 export default defineConfig({
     format: ['cjs', 'esm'],
-    entry: ['./src/index.tsx'],
+    entry: ['./src/index.ts'],
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
